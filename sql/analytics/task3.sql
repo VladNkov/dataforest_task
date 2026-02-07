e7ed6f6d67621b372474a82661cc71eb3c0252bb
VladNkov <vladnkov88@gmail.com>
2026-02-07 16:32:28 +0100

    доданоно прорахунок max збігів для компанії з ds1 в ds2, % збігів для компанії з ds1 2 та більше компаній в ds2 в task3.sql

--- sql/analytics/task3.sql
+++ sql/analytics/task3.sql
@@ -8,7 +8,9 @@
 --            2. % збігів по 3 локаціям
 --            3. % збігів по 2 локаціям
 --            4. % збігів по 1 локації
---            5.
+--            5. max збігів для компанії з ds1 в ds2
+--            6. % збігів для компанії з ds1 2 та більше компаній в ds2
+--            7. % збігів для компанії з ds1 3 та більше компаній в ds2
 
 DROP TABLE IF EXISTS task3;
 
@@ -33,7 +35,11 @@ count_m AS (
         SUM(CASE WHEN best_loc_match = 4 THEN 1 ELSE 0 END) AS four_location_match,
         SUM(CASE WHEN best_loc_match = 3 THEN 1 ELSE 0 END) AS three_location_match,
         SUM(CASE WHEN best_loc_match = 2 THEN 1 ELSE 0 END) AS two_location_match,
-        SUM(CASE WHEN best_loc_match = 1 THEN 1 ELSE 0 END) AS one_location_match
+        SUM(CASE WHEN best_loc_match = 1 THEN 1 ELSE 0 END) AS one_location_match,
+
+        MAX(match_count) AS match_max_company_ds2,
+        SUM(CASE WHEN match_count >= 2 THEN 1 ELSE 0 END) AS match_2plus_company_ds2,
+        SUM(CASE WHEN match_count >= 3 THEN 1 ELSE 0 END) AS match_3plus_company_ds2
     FROM base
 )
 SELECT
@@ -41,6 +47,9 @@ SELECT
     unmatch_company,
     total_company,
     one_to_many_company,
+    match_max_company_ds2,
+    match_2plus_company_ds2,
+    match_3plus_company_ds2,
     ROUND(100.0 * match_company / total_company, 2) AS match_percent,
     ROUND(100.0 * unmatch_company / total_company, 2) AS unmatch_percent,
     ROUND(100.0 * one_to_many_company / total_company, 2) AS one_to_many_percent,
@@ -48,6 +57,11 @@ SELECT
     ROUND(100.0 * four_location_match / total_company, 2) AS four_location_match_persent,
     ROUND(100.0 * three_location_match / total_company, 2) AS three_location_match_persent,
     ROUND(100.0 * two_location_match / total_company, 2) AS two_location_match_persent,
-    ROUND(100.0 * one_location_match / total_company, 2) AS one_location_match_persent
+    ROUND(100.0 * one_location_match / total_company, 2) AS one_location_match_persent,
+
+    ROUND(100.0 * match_2plus_company_ds2 / total_company, 2) AS match_2plus_company_ds2_persent,
+    ROUND(100.0 * match_3plus_company_ds2 / total_company, 2) AS match_3plus_company_ds2_persent
+
+
 FROM count_m;
 
